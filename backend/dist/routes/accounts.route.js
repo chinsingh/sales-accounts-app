d1964ad2101f31e856b207178d83287bc27da3d1
Chinmay Singh <chinsingh@outlook.in>
2025-03-17 03:56:29 +0530

    added extensions

--- backend/dist/routes/accounts.route.js
+++ backend/dist/routes/accounts.route.js
@@ -1,4 +1,4 @@
-import sfAccounts from "../services/sf-accounts.service";
+import sfAccounts from "../services/sf-accounts.service.js";
 import express from "express";
 const router = express.Router();
 router.get("/", async (req, res) => {
