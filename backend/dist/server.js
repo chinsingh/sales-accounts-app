4a698a7883cca27a4ba441a6e6e9a2888a318b79
Chinmay Singh <chinsingh@outlook.in>
2025-03-17 12:24:18 +0530

    fixed endpoints

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import authRouter from "./routes/auth.route.js";
-import accountsRouter from "./routes/accounts.route.js";
-import testRouter from "./routes/test.route.js";
+import authRouter from "./routes/auth.route";
+import accountsRouter from "./routes/accounts.route";
+import testRouter from "./routes/test.route";
 import session from 'express-session';
 import pg from 'connect-pg-simple';
 dotenv.config();
@@ -35,9 +35,9 @@ app.use(session({
     },
     store: sessionStore,
 }));
-app.use("/v1/auth", authRouter);
-app.use("/v1/accounts", accountsRouter);
-app.use("/v1/test", testRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/accounts", accountsRouter);
+app.use("/api/v1/test", testRouter);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //# sourceMappingURL=server.js.map
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import authRouter from "./routes/auth.route";
-import accountsRouter from "./routes/accounts.route";
-import testRouter from "./routes/test.route";
+import authRouter from "./routes/auth.route.js";
+import accountsRouter from "./routes/accounts.route.js";
+import testRouter from "./routes/test.route.js";
 import session from 'express-session';
 import pg from 'connect-pg-simple';
 
@@ -50,9 +50,9 @@ declare module "express-session" {
   }
 }
 
-app.use("/v1/auth", authRouter);
-app.use("/v1/accounts", accountsRouter);
-app.use("/v1/test", testRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/accounts", accountsRouter);
+app.use("/api/v1/test", testRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
