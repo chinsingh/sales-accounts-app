4021d70f39f05f97a8e6aa37c4abbc2e17efadc5
Chinmay Singh <chinsingh@outlook.in>
2025-03-17 10:35:02 +0530

    change backend url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,2 @@
-export const BACKEND_BASE_URL:string = "https://sales-accounts-app.onrender.com";
+export const BACKEND_BASE_URL: string = 'https://sales-accounts-app.vercel.app/'
+// //"https://sales-accounts-app.onrender.com";
